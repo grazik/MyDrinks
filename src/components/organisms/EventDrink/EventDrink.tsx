d01bc630ea9539d7e000d275f8c3cfdb8d8cf8fa
grazik <grazik@gmail.com>
2026-01-29 16:12:43 +0100

    events | polish envents drink styling

--- src/components/organisms/EventDrink/EventDrink.tsx
+++ src/components/organisms/EventDrink/EventDrink.tsx
@@ -2,6 +2,7 @@ import { Drink } from "@prisma/client";
 import Image from "next/image";
 import "./event-drink.scss";
 import { IngredientsSection } from "@/src/components/organisms/IngredientsSection/IngredientsSection";
+import Link from "next/link";
 
 interface EventDrinkProps {
   drink: Drink;
@@ -11,16 +12,20 @@ export const EventDrink = ({ drink }: EventDrinkProps) => {
   return (
     <div className="event-drink">
       <div className="event-drink__section">
-        {drink.image && (
-          <Image
-            className={"event-drink__image"}
-            src={drink.image}
-            alt=""
-            width={683}
-            height={1024}
-          />
-        )}
-        <h3 className="subsection-heading">{drink.name}</h3>
+        <Link href={drink.slug} className="event-drink__link">
+          {drink.image && (
+            <div className="event-drink__image-wrapper">
+              <Image
+                className={"event-drink__image"}
+                src={drink.image}
+                alt=""
+                width={1024}
+                height={1024}
+              />
+            </div>
+          )}
+          <h3 className="subsection-heading">{drink.name}</h3>
+        </Link>
       </div>
 
       <div className="event-drink__section">
