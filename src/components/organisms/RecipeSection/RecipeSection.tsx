afe7f7f559cd0a979b892600d8a1c9fcc9656483
grazik <grazik@gmail.com>
2026-01-30 15:13:20 +0100

    events | update recipe page

--- src/components/organisms/RecipeSection/RecipeSection.tsx
+++ src/components/organisms/RecipeSection/RecipeSection.tsx
@@ -1,16 +1,24 @@
 import { Drink } from "@prisma/client";
 import "./recipe-section.scss";
 import { markdownToHtml } from "@/src/utils/markdown";
+import {
+  H2SectionHeading,
+  SectionHeadingType,
+} from "@/src/components/atoms/SectionHeading/SectionHeading";
 
 type RecipeSectionProps = {
+  Heading?: SectionHeadingType;
   recipe: Drink["recipe"];
 };
 
-export const RecipeSection = async ({ recipe }: RecipeSectionProps) => {
+export const RecipeSection = async ({
+  recipe,
+  Heading = H2SectionHeading,
+}: RecipeSectionProps) => {
   const recipeHTML = await markdownToHtml(recipe);
   return (
     <section className="recipe-section">
-      <h2 className={"section-heading"}>Recipe</h2>
+      <Heading>Recipe</Heading>
       <div
         className={"body-text"}
         dangerouslySetInnerHTML={{ __html: recipeHTML }}
