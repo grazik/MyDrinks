433d819daf87cc853d35768e83bfcf1bd0aad8b9
grazik <30260979+grazik@users.noreply.github.com>
2026-02-19 09:20:42 +0100

    Extract UserDto schema/type and simplify header styles

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -1,16 +1,30 @@
 import { ContentBand } from "@/src/components/atoms/ContentBand/ContentBand";
 import { Logo } from "@/src/components/atoms/Logo/Logo";
+import { signOut } from "@/src/actions/signOut";
+import { getUserDto } from "@/src/actions/getUserDto";
 
 import "./header.scss";
 import { Cta } from "@/src/components/atoms/Cta/Cta";
 
-export const Header = () => {
+export const Header = async () => {
+  const userDto = await getUserDto();
+  const isAuthenticated = Boolean(userDto);
+
   return (
     <ContentBand>
       <header className="header">
         <Logo />
         <div className="header__spacer" />
-        <Cta href="/sign-in">Sign in</Cta>
+        {isAuthenticated ? (
+          // onClick could invoke a server action too, but form action keeps native submit semantics and works without client JS.
+          <form action={signOut}>
+            <Cta type="submit" fill="outline" tone="danger">
+              Sign out
+            </Cta>
+          </form>
+        ) : (
+          <Cta href="/sign-in">Sign in</Cta>
+        )}
       </header>
     </ContentBand>
   );
