95c1eec56ce443cf3e59826a7f0ba5258c8d2e34
grazik <30260979+grazik@users.noreply.github.com>
2026-03-27 12:29:38 +0100

    add images, some polishes (#18)
    Co-authored-by: grazik <grazik@gmail.com>

--- src/app/[...slug]/PdpQuickOrderControls.tsx
+++ src/app/[...slug]/PdpQuickOrderControls.tsx
@@ -1,8 +1,23 @@
+import { getUserDto } from "@/lib/auth/getUserDto";
+import { getActiveEventWithDrinkIds } from "@/db/getEvent";
 import { QuickOrderControls } from "@/src/components/organisms/QuickOrderControls/QuickOrderControls";
 import "./pdp-quick-order-controls.scss";
 
-export const PdpQuickOrderControls = ({ drinkId }: { drinkId: string }) => (
-  <div className="pdp-quick-order-controls">
-    <QuickOrderControls drinkId={drinkId} />
-  </div>
-);
+export const PdpQuickOrderControls = async ({
+  drinkId,
+}: {
+  drinkId: string;
+}) => {
+  const [user, event] = await Promise.all([
+    getUserDto(),
+    getActiveEventWithDrinkIds(),
+  ]);
+
+  if (!user || !event) return null;
+
+  return (
+    <div className="pdp-quick-order-controls">
+      <QuickOrderControls drinkId={drinkId} />
+    </div>
+  );
+};
